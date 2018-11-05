fb9df8dce0c7002e93c7520dd8358d99f67d127a
Perfected1 <chikennamadim65@gmail.com>
2018-11-06 00:48:19 +0100

    checking responsiveness

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ function check() {
 
     function notRepeatedChar(output) {
         //split the users' input into ana arry
-        let arr = output.split('');
-        let resp = '';
-        let rept = 0;
+        var arr = output.split('');
+        var resp = '';
+        var rept = 0;
 
         for (var i = 0; i < arr.length; i++) {
             rept = 0;
@@ -13,17 +13,19 @@ function check() {
                 if (arr[i] === arr[j]) {
                     rept += 1;
                 }
-            }
-            if (rept < 2) {
-                resp = arr[i];
-                break;
-            } else {
-                document.getElementById('output').innerHTML = "All characters are repeated!";
-                break;
-            }
-        }
-        return resp;
+
+                if (rept < 2) {
+                    resp = arr[i];
+                    break;
+                } else {
+                    document.getElementById('output').innerHTML = "All characters are repeated!";
+                }
+                }
+                return resp;
+    
+            
+        };
+    };
+        document.getElementById('output').innerHTML = "The first Non-Repeating Character in the Input given is:" + "<h1>\'<strong>" + notRepeatedChar(userInput) + "\'</strong></h1>";
     }
     
-    document.getElementById('output').innerHTML = "The first Non-Repeating Character in the Input given is:" + "<h1>\'<strong>" + notRepeatedChar(userInput) + "\'</strong></h1>";
-}
