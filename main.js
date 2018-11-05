b84d99624d191bb9f3782107a901ae11c9866597
Perfected1 <chikennamadim65@gmail.com>
2018-11-05 18:37:33 +0100

    add JavaScript, Html and CSS file to my repo
    add JavaScript, Html and CSS file to my repo

---
+++ main.js
@@ -0,0 +1,29 @@
+function check() {
+    const userInput = document.getElementById('input').value;
+
+    function notRepeatedChar(output) {
+        //split the users' input into ana arry
+        let arr = output.split('');
+        let resp = '';
+        let rept = 0;
+
+        for (var i = 0; i < arr.length; i++) {
+            rept = 0;
+            for (var j = 0; j < arr.length; j++) {
+                if (arr[i] === arr[j]) {
+                    rept += 1;
+                }
+            }
+            if (rept < 2) {
+                resp = arr[i];
+                break;
+            } else {
+                document.getElementById('output').innerHTML = "All characters are repeated!";
+                break;
+            }
+        }
+        return resp;
+    }
+    
+    document.getElementById('output').innerHTML = "The first Non-Repeating Character in the Input given is:" + "<h1>\'<strong>" + notRepeatedChar(userInput) + "\'</strong></h1>";
+}
